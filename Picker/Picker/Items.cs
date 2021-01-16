a86672fcce0cf86b778a6e99c6e12bde8fb735b0
master <58977579+alexmasterblack@users.noreply.github.com>
2021-01-17 03:57:31 +1000

    Добавьте файлы проекта.

---
+++ Picker/Picker/Items.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picker
+{
+    public enum Items
+    {
+        RedBook,
+        WhiteCase,
+        BlackNote,
+        OrangeFruit,
+        PurpleFlower,
+        Blue,
+        PinkPigPicture
+    }
+}
---
+++ Picker/Picker/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Picker
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Split(string str)
+        {
+            return Regex.Replace(str, "[A-Z]", " $0").Trim();
+        }
+        private Items selectedItem;
+        public Items SelectedItem
+        {
+            get => selectedItem;
+            set
+            {
+                if (value != selectedItem)
+                {
+                    selectedItem = value;
+                }
+            }
+        }
+        public List<string> ItemsNames
+        {
+            get
+            {
+                return Enum.GetNames(typeof(Items)).Select(b => Split(b)).ToList();
+            }
+        }
+        public string Text { get; set; }
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Selected.Text = "Вы выбрали: " + (sender as Xamarin.Forms.Picker)?.SelectedItem.ToString();
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+}
